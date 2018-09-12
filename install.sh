9cc08c8609dc99d45dec307344f74d56bc3b8ff4
licg <sofunbaba@gmail.com>
2018-09-12 14:16:44 +0800

    sync

--- install.sh
+++ install.sh
@@ -4,23 +4,16 @@
 CURRENT_DIR=`pwd`
 FOR_VIM=true
 
-echo "Step1: backing up current vim config"
-today=`date +%Y%m%d`
+echo "Step1: setting up symlinks"
 if $FOR_VIM; then
-    for i in $HOME/.vim $HOME/.vimrc $HOME/.vimrc.ctags $HOME/.vimrc.bundles $HOME/.tmux.conf; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
-    for i in $HOME/.vim $HOME/.vimrc $HOME/.vimrc.ctags $HOME/.vimrc.bundles $HOME/.tmux.conf; do [ -L $i ] && unlink $i ; done
+    ln -f -s $CURRENT_DIR/vimrc $HOME/.vimrc
+    ln -f -s $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
+    ln -f -s "$CURRENT_DIR/vim" "$HOME/.vim"
+    ln -f -s $CURRENT_DIR/vimrc.ctags $HOME/.vimrc.ctags
+    ln -f -s $CURRENT_DIR/tmux.conf $HOME/.tmux.conf
 fi
 
-echo "Step2: setting up symlinks"
-if $FOR_VIM; then
-    ln -s $CURRENT_DIR/vimrc $HOME/.vimrc
-    ln -s $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
-    ln -s "$CURRENT_DIR/vim" "$HOME/.vim"
-    ln -s $CURRENT_DIR/vimrc.ctags $HOME/.vimrc.ctags
-    ln -s $CURRENT_DIR/tmux.conf $HOME/.tmux.conf
-fi
-
-echo "Step3: update/install plugins using Vim-plug"
+echo "Step2: update/install plugins using Vim-plug"
 system_shell=$SHELL
 export SHELL="/bin/sh"
 if $FOR_VIM; then
